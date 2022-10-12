6a560147354ce53f678389069f9e1e0b83ca2c9a
Satyamisme <satyamisme@gmail.com>
2022-10-12 18:20:02 +0300

    Update leechstart.sh

--- leechstart.sh
+++ leechstart.sh
@@ -4,12 +4,12 @@
 sudo apt install python3 python3-pip
 echo yes | pip3 install -r requirements-cli.txt
 echo yes | pip3 install -r requirements.txt
-#echo yes | sudo docker container prune
+echo yes | sudo docker container prune
 #echo yes | sudo docker image prune -a
 
 sudo dockerd
 
 sudo docker build . -t enc
 
+sudo docker run -ti --cpus="2" --privileged -p 65:65 enc
 
-sudo docker run -ti --privileged -p 65:65 enc
